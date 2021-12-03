b1037b9a93b34c3a7e25d19a0cb6ae3d8e388150
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-03 20:39:03 +0300

    created image and profile models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'pictures/')
+    # image = CloudinaryField('pictures')
+    image_name = models.CharField(max_length =30)
+    image_caption = models.TextField()
+    profile = models.ForeignKey('Location',on_delete=models.CASCADE)
+    likes=models.IntegerField(default=0)
+    comments=models.IntegerField()
+class Profile(models.Model):
+    profile_photo=models.ImageField(upload_to = 'pictures/')
+    bio=models.TextField()
