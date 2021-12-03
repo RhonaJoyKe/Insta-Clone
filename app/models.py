8369f48a7efaf4ffee1f5aee6d9433121867ec1b
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-04 01:05:02 +0300

    adds the comments and likes models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -6,9 +8,20 @@ class Image(models.Model):
     # image = CloudinaryField('pictures')
     image_name = models.CharField(max_length =30)
     image_caption = models.TextField()
-    profile = models.ForeignKey('Location',on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile',on_delete=models.CASCADE)
     likes=models.IntegerField(default=0)
     comments=models.IntegerField()
 class Profile(models.Model):
     profile_photo=models.ImageField(upload_to = 'pictures/')
     bio=models.TextField()
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=50)
+    comment_date = models.DateTimeField(auto_now_add=True)
+class Likes(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    # likes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
