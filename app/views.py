3525926d475e800ba54175fae4888d62921355d6
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-04 21:00:19 +0300

    adds search function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import datetime as dt
+from .models import Image,Profile,Likes,Comments
+from django.http  import HttpResponse,Http404
+
 
 # Create your views here.
+def welcome(request):
+    
+   
+    return render(request,'home.html')
+def search_results(request):
+
+    if 'name' in request.GET and request.GET["name"]:
+        search_term = request.GET.get("name")
+        searched_images = Image.search_image(search_term)
+        print(searched_images)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
