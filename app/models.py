71a3b5363e781376ce068ea131a7ecce643b1b5b
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-04 19:32:05 +0300

    adds models

--- app/models.py
+++ app/models.py
@@ -6,22 +6,27 @@ from django.contrib.auth.models import User
 class Image(models.Model):
     image = models.ImageField(upload_to = 'pictures/')
     # image = CloudinaryField('pictures')
-    image_name = models.CharField(max_length =30)
-    image_caption = models.TextField()
+    name = models.CharField(max_length =30)
+    caption = models.TextField()
     profile = models.ForeignKey('Profile',on_delete=models.CASCADE)
-    likes=models.IntegerField(default=0)
-    comments=models.IntegerField()
+    
+    def __str__(self):
+        return self.image_name
 class Profile(models.Model):
     profile_photo=models.ImageField(upload_to = 'pictures/')
     bio=models.TextField()
     user=models.OneToOneField(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.bio
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=50)
     comment_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.comment
 class Likes(models.Model):
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    # likes = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.image
 
