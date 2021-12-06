46d97aaf09afd2d58696008243563e157369c849
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-06 12:00:24 +0300

    adds profile image

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ import datetime as dt
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.forms import ModelForm
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 
@@ -50,6 +52,14 @@ class Profile(models.Model):
      # delete profile from database
     def delete_profile(self):
         self.delete()
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
