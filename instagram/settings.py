9b53dc3bec9ff9024da53a7d5c72f8f1cb7229a1
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-08 13:53:51 +0300

    changes profile template

--- instagram/settings.py
+++ instagram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-MODE=config("MODE", default="dev")
+MODE=config="dev"
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
